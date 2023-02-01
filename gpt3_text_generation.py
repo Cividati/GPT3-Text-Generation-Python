5ca31c846b1506ac7985259da5585ede6bf9ca95
Rubens Cividati <r.cividati@4intelligence.com.br>
2023-02-01 16:52:27 -0300

    feat: add cache mechanism

--- gpt3_text_generation.py
+++ gpt3_text_generation.py
@@ -4,7 +4,15 @@ import openai
 # Read the API key from an environment variable
 openai.api_key = os.environ.get("API_KEY")
 
+# Cache to store the results of previous function calls
+cache = {}
+
 def generate_text(prompt):
+    # Check if the prompt is in the cache
+    if prompt in cache:
+        return cache[prompt]
+
+    # Make a new API call if the prompt is not in the cache
     completions = openai.Completion.create(
         engine="text-davinci-002",
         prompt=prompt,
@@ -15,6 +23,10 @@ def generate_text(prompt):
     )
 
     message = completions.choices[0].text
+    
+    # Store the result in the cache
+    cache[prompt] = message
+    
     return message
 
 generated_text = generate_text("What is the meaning of life?")
